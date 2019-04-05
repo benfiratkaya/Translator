4e5a0593968f9ca71c47c2cb405c627d9605f22f
Fırat Kaya <48600092+benfiratkaya@users.noreply.github.com>
2019-04-05 21:21:44 +0300

    Create translate_variable.php

---
+++ examples/translate_variable.php
@@ -0,0 +1,31 @@
+<?php
+	use Translator\Translator;
+	use Translator\Exception;
+	
+	// Composer
+	require '../vendor/autoload.php';
+	
+	// Include
+	//include_once 'translator/Exception.php';
+	//include_once 'translator/Register.php';
+	//include_once 'translator/Generator.php';
+	//include_once 'translator/Translator.php';
+		
+	// Exception, Type, Language, Path
+	$translator = new Translator(true, 'json', 'es_ES', 'languages/json');
+		
+	// Register Functions: translate(), translator(), t__(), e__()
+	$translator->register();
+		
+	$translator->setException(true); // true or false
+		
+	// path/lang.type -> /languages/en_US.json
+	$translator->setType('json'); // php, json, ini
+	$translator->setLang('es_ES'); // Language Code.
+	$translator->setPath('languages/json'); // Language Files Directory
+		
+	// Update Changes
+	$translator->update();
+	
+	$user = 'Firat Kaya';
+	echo $translator->translate('Hello %user%', array('%user%' => $user)); // Output: Hola Firat Kaya
